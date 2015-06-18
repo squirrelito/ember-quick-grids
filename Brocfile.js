ac30a9bb3dda83f38694833eb884eb3053c61af0
SquirreLito <g.budinov@gmail.com>
2015-06-19 01:39:40 +0300

    Init js

--- Brocfile.js
+++ Brocfile.js
@@ -3,14 +3,14 @@
 
 var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
-/*
-  This Brocfile specifes the options for the dummy test app of this
-  addon, located in `/tests/dummy`
-
-  This Brocfile does *not* influence how the addon or the app using it
-  behave. You most likely want to be modifying `./index.js` or app's Brocfile
-*/
-
 var app = new EmberAddon();
+
+app.import('bower_components/ember/ember-template-compiler.js');
+app.import('bower_components/jquery-ui/jquery-ui.min.js');
+app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
+app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
+app.import('bower_components/jquery.sorttable/index.js');
+app.import('bower_components/jquery-resizable-columns/dist/jquery.resizableColumns.min.js');
+app.import('bower_components/jquery-resizable-columns/dist/jquery.resizableColumns.css');
 
 module.exports = app.toTree();
