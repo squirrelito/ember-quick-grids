e7cc0f9122f98196f698e6d171cef2f6d6a573c8
SquirreLito <g.budinov@gmail.com>
2015-10-02 20:17:17 +0300

    Ember 2.0 and blueprints

--- index.js
+++ index.js
@@ -3,19 +3,28 @@
 
 module.exports = {
   name: 'ember-quick-grids',
-  included: function (app) {
+  included: function (app, parentAddon) {
     this._super.included(app);
 
-    app.import(app.bowerDirectory + '/ember/ember-template-compiler.js');
+    var target = (parentAddon || app);
 
-    app.import(app.bowerDirectory + '/jquery-ui/jquery-ui.min.js');
-    app.import(app.bowerDirectory + '/bootstrap/dist/js/bootstrap.min.js');
-    //app.import(app.bowerDirectory + '/jquery.sorttable/index.js');
-    app.import('vendor/sorttable/index.js');
-    app.import(app.bowerDirectory + '/jquery-resizable-columns/dist/jquery.resizableColumns.min.js');
+    target.import(target.bowerDirectory + '/ember/ember-template-compiler.js');
 
-    app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.min.css');
-    app.import(app.bowerDirectory + '/fontawesome/css/font-awesome.min.css');
-    app.import(app.bowerDirectory + '/jquery-resizable-columns/dist/jquery.resizableColumns.css');
+    target.import(target.bowerDirectory + '/jquery-ui/jquery-ui.min.js');
+    target.import(target.bowerDirectory + '/bootstrap/dist/js/bootstrap.min.js');
+    //target.import(target.bowerDirectory + '/jquery.sorttable/index.js');
+    target.import('vendor/sorttable/index.js');
+    target.import(target.bowerDirectory + '/jquery-resizable-columns/dist/jquery.resizableColumns.min.js');
+
+    target.import(target.bowerDirectory + '/bootstrap/dist/css/bootstrap.min.css');
+    target.import(target.bowerDirectory + '/jquery-resizable-columns/dist/jquery.resizableColumns.css');
+
+    target.import(target.bowerDirectory + '/fontawesome/css/font-awesome.min.css');
+    target.import(target.bowerDirectory + "/fontawesome/fonts/fontawesome-webfont.eot", { destDir: "fonts" });
+    target.import(target.bowerDirectory + "/fontawesome/fonts/fontawesome-webfont.svg", { destDir: "fonts" });
+    target.import(target.bowerDirectory + "/fontawesome/fonts/fontawesome-webfont.ttf", { destDir: "fonts" });
+    target.import(target.bowerDirectory + "/fontawesome/fonts/fontawesome-webfont.woff", { destDir: "fonts" });
+    target.import(target.bowerDirectory + "/fontawesome/fonts/fontawesome-webfont.woff2", { destDir: "fonts" });
+    target.import(target.bowerDirectory + "/fontawesome/fonts/FontAwesome.otf", { destDir: "fonts" });
   }
 };
