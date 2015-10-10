0f8e9b78f0328a8a4eeb3d49457ad2b07368c575
SquirreLito <g.budinov@gmail.com>
2015-10-10 17:57:17 +0300

    mirage namespace

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -39,7 +39,7 @@ module.exports = function(environment) {
     ENV.APP.rootElement = '#ember-testing';
   }
 
-  if (environment === 'production') {
+  if (environment === 'gh-pages') {
     ENV.baseURL = '/ember-quick-grids/'
   }
 
